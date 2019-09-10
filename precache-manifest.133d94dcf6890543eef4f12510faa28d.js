f1b4a94942967b0c27f82cf9503b6bef57b69aa8
David Fischer <david@simustream.com>
2019-09-10 09:14:02 -0600

    Updates

---
+++ precache-manifest.133d94dcf6890543eef4f12510faa28d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e9161962713d8c2ca893f0b4bfe317",
+    "url": "/ssadmin/index.html"
+  },
+  {
+    "revision": "5692c9aca05869675d49",
+    "url": "/ssadmin/static/css/main.33847a5f.chunk.css"
+  },
+  {
+    "revision": "b84f43d7cc66fe45dfcb",
+    "url": "/ssadmin/static/js/2.2bcb5db6.chunk.js"
+  },
+  {
+    "revision": "5692c9aca05869675d49",
+    "url": "/ssadmin/static/js/main.14dc4e9a.chunk.js"
+  },
+  {
+    "revision": "c48f37175cab5dd6dc57",
+    "url": "/ssadmin/static/js/runtime~main.9edbe7e3.js"
+  }
+]);
