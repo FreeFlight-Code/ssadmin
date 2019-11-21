628eee30e727a52935723cc251ed3ded7f9ef907
David Fischer <david@simustream.com>
2019-11-21 11:32:34 -0700

    removed 2 links in menu

--- src/routes.js
+++ src/routes.js
@@ -19,9 +19,9 @@ import Dashboard from "views/Dashboard.jsx";
 // import Icons from "views/Icons.jsx";
 // import Map from "views/Map.jsx";
 // import Notifications from "views/Notifications.jsx";
-import TableList from "views/TableList.jsx";
+// import TableList from "views/TableList.jsx";
 // import Typography from "views/Typography.jsx";
-import UserProfile from "views/UserProfile.jsx";
+// import UserProfile from "views/UserProfile.jsx";
 
 var routes = [
   {
@@ -71,22 +71,22 @@ var routes = [
   //   component: Notifications,
   //   layout: "/admin"
   // },
-  {
-    path: "/user-profile",
-    name: "User Profile",
-    rtlName: "ملف تعريفي للمستخدم",
-    icon: "tim-icons icon-single-02",
-    component: UserProfile,
-    layout: "/admin"
-  },
-  {
-    path: "/tables",
-    name: "Table List",
-    rtlName: "قائمة الجدول",
-    icon: "tim-icons icon-puzzle-10",
-    component: TableList,
-    layout: "/admin"
-  },
+  // {
+  //   path: "/user-profile",
+  //   name: "User Profile",
+  //   rtlName: "ملف تعريفي للمستخدم",
+  //   icon: "tim-icons icon-single-02",
+  //   component: UserProfile,
+  //   layout: "/admin"
+  // },
+  // {
+  //   path: "/tables",
+  //   name: "Table List",
+  //   rtlName: "قائمة الجدول",
+  //   icon: "tim-icons icon-puzzle-10",
+  //   component: TableList,
+  //   layout: "/admin"
+  // },
   // {
   //   path: "/typography",
   //   name: "Typography",
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -178,15 +178,15 @@ class Dashboard extends React.Component {
 
 export default Dashboard;
 
-function AddPropsHOC(WrappedComponent) {
-  return class extends React.Component {
-      componentWillReceiveProps(nextProps) {
-      console.log('Current props: ', this.props);
-      console.log('Next props: ', nextProps);
-      }
-      render() {
-      // Wraps the input component in a container, without mutating it. Good!
-      return <WrappedComponent {...this.props} />;
-      }
-  }
-}
+// function AddPropsHOC(WrappedComponent) {
+//   return class extends React.Component {
+//       componentWillReceiveProps(nextProps) {
+//       console.log('Current props: ', this.props);
+//       console.log('Next props: ', nextProps);
+//       }
+//       render() {
+//       // Wraps the input component in a container, without mutating it. Good!
+//       return <WrappedComponent {...this.props} />;
+//       }
+//   }
+// }
